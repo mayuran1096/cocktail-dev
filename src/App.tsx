c47f546d7a060d893d7d1feb1da1992da5721cba
mayuranmayuran-knod-dev <mayuran@knod.sg>
2022-11-25 09:24:41 +0530

    random cocktails done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useMemo } from "react";
+import "./App.css";
+import axios from "axios";
+
+interface singleCocktailType {
+  strDrink: string;
+  strDrinkThumb: string;
+  strCategory: string;
+  strGlass: string;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [randomCocktails, setRandomCockTails] = useState<
+    singleCocktailType[] | []
+  >([]);
+
+  async function fetchData() {
+    console.log("calling");
+
+    const cocktailRandomList = await Promise.all([
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`),
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`),
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`),
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`),
+      axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`),
+    ]);
+    const filterCockTailList = cocktailRandomList?.map(
+      (cocktail: any, index: Number) => {
+        const { data } = cocktail;
+        return data?.drinks?.[0] || [];
+      }
+    );
+    console.log(filterCockTailList, "www");
+    setRandomCockTails(filterCockTailList || []);
+  }
+
+  const renderRandomCockTails = useMemo(() => {
+    return randomCocktails?.map(
+      (cocktail: singleCocktailType, index: number) => {
+        return <div key={index}>{cocktail?.strDrink || ""}</div>;
+      }
+    );
+  }, [randomCocktails]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div className="App">{renderRandomCockTails}</div>;
 }
 
 export default App;
